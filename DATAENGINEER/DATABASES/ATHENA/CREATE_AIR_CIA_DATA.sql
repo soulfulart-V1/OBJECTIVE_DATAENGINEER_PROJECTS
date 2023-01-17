13425fd400bc5057228b55d08009d3047a2ef50e
Paulo Melo <paulofariamelo@gmail.com>
2023-01-17 08:54:53 -0300

    add athena table AIR_CIA

--- DATAENGINEER/DATABASES/ATHENA/CREATE_AIR_CIA_DATA.sql
+++ DATAENGINEER/DATABASES/ATHENA/CREATE_AIR_CIA_DATA.sql
@@ -1,22 +1,19 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS AERO.IOT_MESSAGES_REGION_A_BATCH (
-
-    razão_social STRING,
+CREATE EXTERNAL TABLE IF NOT EXISTS OBJECTIVE_AERODROMO.AIR_CIA (
+    razao_social STRING,
     cnpj STRING,
-    atividades_aéreas STRING,
-    endereço_sede STRING,
+    atividades_aereas STRING,
+    endereco_sede STRING,
     telefone STRING,
-    e-mail STRING,
-    decisão_operacional STRING,
-    data_decisão_operacional STRING,
+    email STRING,
+    decisao_operacional STRING,
+    data_decisao_operacional STRING,
     validade_operacional STRING,
     icao STRING,
     iata STRING
-
     )
 
 PARTITIONED BY(yearmonthday int)
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY ','
-STORED AS CSV
 
 LOCATION 's3://objective-data-lake/CURATED/DEPARTMENT_AERODROMO/PROJECT_445798_AIRLINE-VRA-AIR/AIR_CIA/'
