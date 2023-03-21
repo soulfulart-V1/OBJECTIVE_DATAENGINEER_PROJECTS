e6d710dc58823d60077d8a12f99ba076ab4565df
Paulo Melo <paulofariamelo@gmail.com>
2023-03-21 11:57:12 -0300

    MOST_USED_ROUTE_BY_AIRCOMPANY created!

--- DATAENGINEER/DATABASES/ATHENA/DDL/CREATE_VIEW_MAX_ROUTE.sql
+++ DATAENGINEER/DATABASES/ATHENA/DDL/CREATE_VIEW_MAX_ROUTE.sql
@@ -1,40 +1,91 @@
-SELECT emp_max_count.icao_empr, emp_max_count.max_voo_count, emp_voo_count.num_voo
+CREATE OR REPLACE VIEW MOST_USED_ROUTE_BY_AIRCOMPANY AS
 
-FROM
+WITH max_icao_flight AS (
 
---table emp_max_count
-(
+SELECT icao_empr, num_voo FROM (
 
-SELECT icao_empr, MAX(num_voo_count) AS max_voo_count FROM  (
 SELECT
+
 "icaoempresaaérea" as icao_empr,
 "númerovoo" as num_voo,
-COUNT("númerovoo") as num_voo_count
+COUNT("númerovoo") as num_voo_count,
+RANK() OVER(PARTITION BY "icaoempresaaérea" ORDER BY COUNT("númerovoo") DESC) Rank 
+
 FROM objective_aerodromo.vra
-AS emp_voo_count
+WHERE "situaçãovoo" = 'REALIZADO'
 GROUP BY "icaoempresaaérea", "númerovoo"
 ORDER BY num_voo_count DESC
 
 )
 
-GROUP BY icao_empr
+WHERE Rank=1
 
-) emp_max_count
+),
 
-LEFT JOIN
+icao_flight_filter_max AS (
 
---emp_voo_count
-(
-SELECT
+SELECT DISTINCT
+
 "icaoempresaaérea" as icao_empr,
-"númerovoo" as num_voo,
-COUNT("númerovoo") as num_voo_count
+"icaoaeródromoorigem",
+"icaoaeródromodestino"
+
 FROM objective_aerodromo.vra
-AS emp_voo_count
-GROUP BY "icaoempresaaérea", "númerovoo"
-ORDER BY num_voo_count DESC
-) emp_voo_count
+
+WHERE "númerovoo" in (SELECT num_voo FROM max_icao_flight)
+
+),
+
+icao_flight_filter_max_air_name AS (
+
+SELECT
+
+icao_flight_filter_max.icao_empr,
+icao_flight_filter_max."icaoaeródromoorigem",
+icao_flight_filter_max."icaoaeródromodestino",
+air_cia.razao_social
+
+FROM icao_flight_filter_max
+
+LEFT JOIN air_cia
+
+ON icao_flight_filter_max.icao_empr = air_cia.icao
+
+),
+
+icao_flight_filter_max_air_name_origem AS (
+
+SELECT 
+
+icao_flight_filter_max_air_name.razao_social,
+icao_flight_filter_max_air_name."icaoaeródromoorigem",
+icao_name_list.name AS "name_aeroporto_origem",
+icao_name_list.state AS "estado_aeroporto_origem",
+icao_flight_filter_max_air_name."icaoaeródromodestino"
+
+FROM icao_flight_filter_max_air_name
+
+LEFT JOIN icao_name_list
+
+ON icao_flight_filter_max_air_name."icaoaeródromoorigem" = icao_name_list.icao
+
+),
+
+final_table_view AS (
+
+SELECT
+icao_flight_filter_max_air_name_origem.*,
+icao_name_list.name AS "name_aeroporto_destino",
+icao_name_list.state AS "estado_aeroporto_destino"
+
+FROM icao_flight_filter_max_air_name_origem
+
+LEFT JOIN icao_name_list
 
 ON
 
-(emp_max_count.max_voo_count = emp_voo_count.num_voo_count)
+icao_flight_filter_max_air_name_origem."icaoaeródromodestino" = icao_name_list.icao
+
+)
+
+SELECT * FROM final_table_view;
